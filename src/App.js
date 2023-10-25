161b409cde8b567375921bd10fc00494c4fc708b
sowmya <sowmyakv22@gmail.com>
2023-10-25 14:31:53 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import SignUp from "./screens/SignUp/Signup";
+import SignIn from "./screens/SignIn/SignIn";
+import Link from "@mui/material/Link";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./screens/Home/Home";
+import ProjectDetails from "./screens/ProjectDetails/ProjectDetails";
+import AboutUs from "./AboutUs";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignIn/>,
+  },
+  {
+    path: "/about-us",
+    element: <AboutUs/>,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp/>,
+  },
+  {
+    path: "/home",
+    element: <Home/>,
+  },
+  {
+    path: "/details/:name",
+    element: <ProjectDetails/>,
+  },
+])
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
